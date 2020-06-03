ef0eeea336f96b35546d57552cffff8b331af174
alolika1998 <66350376+alolika1998@users.noreply.github.com>
2020-06-03 17:43:36 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+)
+func printBytes(s string) {
+    fmt.Printf("Bytes: ")
+    for i:= 0; i< len(s); i++ {
+        fmt.Printf("%x ", s[i])
+    }
+}
+func printChars(s string) {
+    fmt.Printf("Characters: ")
+    for i:= 0; i< len(s); i++ {
+        fmt.Printf("%c ", s[i])
+    }
+}
+
+func main() {
+    name:= "HELLO WORLD"
+    fmt.Printf("String: %s\n", name)
+    printChars(name)
+    fmt.Printf("\n")
+    printBytes(name)
+}
